5b9363d1d134644c16ffe1be417f1c12cbcb4566
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-10-26 18:29:35 +0200

    Add cache for reverse DNS to avoid overloading DNS server.

--- gss/src/main/java/org/globus/gsi/gssapi/GlobusGSSName.java
+++ gss/src/main/java/org/globus/gsi/gssapi/GlobusGSSName.java
@@ -14,6 +14,9 @@
  */
 package org.globus.gsi.gssapi;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -28,228 +31,252 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 /**
- * An implementation of <code>GSSName</code>.
+ * An implementation of
+ * <code>GSSName</code>.
  */
 public class GlobusGSSName implements GSSName, Serializable {
 
+    public static int CacheSize = 10000;
+    public static int CacheTime = 60;
+    
+    private static LoadingCache<String, String> resolved =
+            CacheBuilder.newBuilder().
+            maximumSize(CacheSize).
+            expireAfterWrite(CacheTime, TimeUnit.MINUTES).build(
+            new CacheLoader<String, String>() {
+                public String load(String name) throws UnknownHostException {
+                    // performs reverse DNS lookup
+                    InetAddress i = InetAddress.getByName(name);
+                    String host = InetAddress.getByName(i.getHostAddress()).getHostName();
+                    if (isIPAddress(host)) {
+                        throw new UnknownHostException(host);
+                    }
+                    return host;
+                }
+            });
+    
+    private static Pattern ipPattern = Pattern.compile("\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+
+    private static boolean isIPAddress(String str) {
+        return ipPattern.matcher(str).matches();
+    }
+    
     protected Oid nameType;
     protected X500Principal name;
-
     // set toString called
     protected String globusID;
-
     // set when constructing with GSSName.NT_HOSTBASED_SERVICE as name type
     // or in the getter
     protected String hostBasedServiceCN;
 
     public GlobusGSSName() {
-	this.nameType = GSSName.NT_ANONYMOUS;
-	this.name = null;
+        this.nameType = GSSName.NT_ANONYMOUS;
+        this.name = null;
     }
 
     public GlobusGSSName(X500Principal name) {
-	if (name == null) {
-	    this.nameType = GSSName.NT_ANONYMOUS;
-	}
-	this.name = name;
+        if (name == null) {
+            this.nameType = GSSName.NT_ANONYMOUS;
+        }
+        this.name = name;
     }
 
     public GlobusGSSName(byte[] name) {
-	if (name == null) {
-	    this.nameType = GSSName.NT_ANONYMOUS;
-	    this.name = null;
-	} else {
-	    this.name = new X500Principal(name);
-	}
+        if (name == null) {
+            this.nameType = GSSName.NT_ANONYMOUS;
+            this.name = null;
+        } else {
+            this.name = new X500Principal(name);
+        }
     }
-    
+
     /**
      * Creates name from Globus DN
      *
-     * @param name Globus DN (e.g. /C=US/O=Globus/..) If null
-     *        it is considered set as <code>GSSName.ANONYMOUS</code> name type.
+     * @param name Globus DN (e.g. /C=US/O=Globus/..) If null it is considered
+     * set as <code>GSSName.ANONYMOUS</code> name type.
      */
-    public GlobusGSSName(String name) 
-	throws GSSException {
-	if (name == null) {
-	    this.nameType = GSSName.NT_ANONYMOUS;
-	    this.name = null;
-	} else {
-	    try {
-		this.name = CertificateUtil.toPrincipal(name);
-	    } catch (Exception e) {
-		throw new GlobusGSSException(GSSException.BAD_NAME, e);
-	    }
-	}
+    public GlobusGSSName(String name)
+            throws GSSException {
+        if (name == null) {
+            this.nameType = GSSName.NT_ANONYMOUS;
+            this.name = null;
+        } else {
+            try {
+                this.name = CertificateUtil.toPrincipal(name);
+            } catch (Exception e) {
+                throw new GlobusGSSException(GSSException.BAD_NAME, e);
+            }
+        }
     }
 
     /**
      * Creates name from X509 name of specified type.
      *
-     * @param name 
-     *        Globus DN (e.g. /C=US/O=Globus/..) or service@host name. If null
-     *        it is considered set as <code>GSSName.ANONYMOUS</code> name type.
-     * @param nameType name type. Only <code>GSSName.NT_ANONYMOUS</code> 
-     *                 or <code>GSSName.NT_HOSTBASED_SERVICE</code> is supported.
-     *                 Maybe be null.
+     * @param name Globus DN (e.g. /C=US/O=Globus/..) or service@host name. If
+     * null it is considered set as <code>GSSName.ANONYMOUS</code> name type.
+     * @param nameType name type. Only <code>GSSName.NT_ANONYMOUS</code>        or <code>GSSName.NT_HOSTBASED_SERVICE</code> is
+     * supported. Maybe be null.
      */
     public GlobusGSSName(String name, Oid nameType)
-	throws GSSException {
-	if (name == null) {
-	    if (nameType != null && !nameType.equals(GSSName.NT_ANONYMOUS)) {
-		throw new GSSException(GSSException.BAD_NAMETYPE);
-	    } 
-	    this.name = null;
-	    this.nameType = GSSName.NT_ANONYMOUS;
-	} else {
-	    if (nameType != null) {
-		if (nameType.equals(GSSName.NT_HOSTBASED_SERVICE)) {
-		    int atPos = name.indexOf('@');
-		    if (atPos == -1 || (atPos+1 >= name.length())) {
-			throw new GlobusGSSException(GSSException.FAILURE,
-						     GlobusGSSException.BAD_NAME,
-						     "badName00");
-		    }
-		    // performs reverse DNS lookup
-		    String host = name.substring(atPos+1);
-		    try {
-			InetAddress i = InetAddress.getByName(host);
-			host = InetAddress.getByName(i.getHostAddress()).getHostName();
-		    } catch (UnknownHostException e) {
-			throw new GlobusGSSException(GSSException.FAILURE, e);
-		    }
+            throws GSSException {
+        if (name == null) {
+            if (nameType != null && !nameType.equals(GSSName.NT_ANONYMOUS)) {
+                throw new GSSException(GSSException.BAD_NAMETYPE);
+            }
+            this.name = null;
+            this.nameType = GSSName.NT_ANONYMOUS;
+        } else {
+            if (nameType != null) {
+                if (nameType.equals(GSSName.NT_HOSTBASED_SERVICE)) {
+                    int atPos = name.indexOf('@');
+                    if (atPos == -1 || (atPos + 1 >= name.length())) {
+                        throw new GlobusGSSException(GSSException.FAILURE,
+                                GlobusGSSException.BAD_NAME,
+                                "badName00");
+                    }
+                    
+                    // performs reverse DNS lookup
+                    String host;
+                    try {
+                        host = resolved.get(name.substring(atPos+1));
+                    } catch (ExecutionException e) {
+                        throw new GlobusGSSException(GSSException.FAILURE, e);
+                    }
 
-            hostBasedServiceCN = name.substring(0, atPos) + "/" + host;
-		    this.name = new X500Principal("CN=" + hostBasedServiceCN);
-		} else {
-		    throw new GSSException(GSSException.BAD_NAMETYPE);
-		}
-	    } else {
-		try {
-		    this.name = CertificateUtil.toPrincipal(name);
-		} catch (Exception e) {
-		    throw new GlobusGSSException(GSSException.BAD_NAME, e);
-		}
-	    }
-	    this.nameType = nameType;
-	}
-	// both subject & nameType might be null
+                    hostBasedServiceCN = name.substring(0, atPos) + "/" + host;
+                    this.name = new X500Principal("CN=" + hostBasedServiceCN);
+                } else {
+                    throw new GSSException(GSSException.BAD_NAMETYPE);
+                }
+            } else {
+                try {
+                    this.name = CertificateUtil.toPrincipal(name);
+                } catch (Exception e) {
+                    throw new GlobusGSSException(GSSException.BAD_NAME, e);
+                }
+            }
+            this.nameType = nameType;
+        }
+        // both subject & nameType might be null
     }
-    
+
     public boolean isAnonymous() {
-	return (this.name == null);
+        return (this.name == null);
     }
-    
+
     public boolean isMN() {
-	return true;
+        return true;
     }
 
     public boolean equals(GSSName another)
-	throws GSSException {
-	if (another == null) {
-	    return false;
-	}
+            throws GSSException {
+        if (another == null) {
+            return false;
+        }
 
-	if (isAnonymous()) {
-	    return another.isAnonymous();
-	} 
+        if (isAnonymous()) {
+            return another.isAnonymous();
+        }
 
-	if (another.isAnonymous()) {
-	    return false;
-	}
+        if (another.isAnonymous()) {
+            return false;
+        }
 
-	if (!(another instanceof GlobusGSSName)) {
-	    throw new GSSException(GSSException.FAILURE);
-	}
-	
-	GlobusGSSName other = (GlobusGSSName)another;
+        if (!(another instanceof GlobusGSSName)) {
+            throw new GSSException(GSSException.FAILURE);
+        }
 
-	// both are not anonymous
-	// both have non-null subjects
-	// nametypes might be different! (null)
+        GlobusGSSName other = (GlobusGSSName) another;
 
-	if ((nameType != null && nameType.equals(GSSName.NT_HOSTBASED_SERVICE)) ||
-	    (other.nameType != null && other.nameType.equals(GSSName.NT_HOSTBASED_SERVICE))) {
-	    // perform host based comparison
-	
-	    String hp1 = this.getHostBasedServiceCN(true);
-	    String hp2 = other.getHostBasedServiceCN(true);
+        // both are not anonymous
+        // both have non-null subjects
+        // nametypes might be different! (null)
 
-	    if (hp1 == null || hp2 == null) {
-		// something is really wrong
-		return false;
-	    }
-	    
-	    String service1 = getService(hp1);
-	    String service2 = getService(hp2);
+        if ((nameType != null && nameType.equals(GSSName.NT_HOSTBASED_SERVICE))
+                || (other.nameType != null && other.nameType.equals(GSSName.NT_HOSTBASED_SERVICE))) {
+            // perform host based comparison
 
-	    // service types do not match
-	    if (!service1.equalsIgnoreCase(service2)) {
-		return false;
-	    }
+            String hp1 = this.getHostBasedServiceCN(true);
+            String hp2 = other.getHostBasedServiceCN(true);
 
-	    String host1 = getHost(hp1);
-	    String host2 = getHost(hp2);
+            if (hp1 == null || hp2 == null) {
+                // something is really wrong
+                return false;
+            }
 
-	    int i1=0;
-	    int i2=0;
-	    int s1 = host1.length();
-	    int s2 = host2.length();
-	    char h1;
-	    char h2;
-	    while (i1 < s1 && i2 < s2) {
-		h1 = Character.toUpperCase(host1.charAt(i1));
-		h2 = Character.toUpperCase(host2.charAt(i2));
+            String service1 = getService(hp1);
+            String service2 = getService(hp2);
 
-		if (h1 == h2) {
-		    if (h1 == '.') {
-			return host1.equalsIgnoreCase(host2);
-		    }
-		    i1++;
-		    i2++;
-		} else if (h1 == '.' && h2 == '-') {
-		    return compareHost(host2, i2, host1, i1);
-		} else if (h1 == '-' && h2 == '.') {
-		    return compareHost(host1, i1, host2, i2);
-		} else {
-		    return false;
-		}
-	    }
-	    return (i1 == i2);
+            // service types do not match
+            if (!service1.equalsIgnoreCase(service2)) {
+                return false;
+            }
 
-	} else {
-	    // perform regular comparison
+            String host1 = getHost(hp1);
+            String host2 = getHost(hp2);
 
-	    // cross-check getStringNameType()
-	    // that's not implemented right now
+            int i1 = 0;
+            int i2 = 0;
+            int s1 = host1.length();
+            int s2 = host2.length();
+            char h1;
+            char h2;
+            while (i1 < s1 && i2 < s2) {
+                h1 = Character.toUpperCase(host1.charAt(i1));
+                h2 = Character.toUpperCase(host2.charAt(i2));
 
-	    return toString().equalsIgnoreCase(another.toString());
-	}
+                if (h1 == h2) {
+                    if (h1 == '.') {
+                        return host1.equalsIgnoreCase(host2);
+                    }
+                    i1++;
+                    i2++;
+                } else if (h1 == '.' && h2 == '-') {
+                    return compareHost(host2, i2, host1, i1);
+                } else if (h1 == '-' && h2 == '.') {
+                    return compareHost(host1, i1, host2, i2);
+                } else {
+                    return false;
+                }
+            }
+            return (i1 == i2);
+
+        } else {
+            // perform regular comparison
+
+            // cross-check getStringNameType()
+            // that's not implemented right now
+
+            return toString().equalsIgnoreCase(another.toString());
+        }
     }
 
     /**
-     * Returns globus ID string representation of the name.
-     * If name represents is an anonymous name string
-     * "<anonymous>" is returned.
+     * Returns globus ID string representation of the name. If name represents
+     * is an anonymous name string "<anonymous>" is returned.
      */
     public String toString() {
-	if (this.name == null) {
-	    return "<anonymous>";
-	} else {
-	    if (this.globusID == null) {
-		this.globusID = CertificateUtil.toGlobusID(name);
-	    }
-	    return this.globusID;
-	}
+        if (this.name == null) {
+            return "<anonymous>";
+        } else {
+            if (this.globusID == null) {
+                this.globusID = CertificateUtil.toGlobusID(name);
+            }
+            return this.globusID;
+        }
     }
 
     /**
      * Returns the CN corresponding to the host part of the DN
-     * @param last true if the CN is assumed to be the last CN attribute
-     * in the RFC 2253 formatted DN, else false to assume it is the first DN
-     * attribute
+     *
+     * @param last true if the CN is assumed to be the last CN attribute in the
+     * RFC 2253 formatted DN, else false to assume it is the first DN attribute
      * @return the CN of the host based service
      */
     protected String getHostBasedServiceCN(boolean last) {
@@ -289,62 +316,61 @@ public class GlobusGSSName implements GSSName, Serializable {
     }
 
     private static String getService(String name) {
-	int pos = name.indexOf('/');
-	return (pos == -1) ? "host" : name.substring(0, pos);
-    }
-    
-    private static String getHost(String name) {
-	int pos = name.indexOf('/');
-	return (pos == -1) ? name : name.substring(pos+1);
+        int pos = name.indexOf('/');
+        return (pos == -1) ? "host" : name.substring(0, pos);
     }
 
-    private static boolean compareHost(String host1, int i, 
-				       String host2, int j) {
-	if (host1.charAt(i) != '-') {
-	    throw new IllegalArgumentException();
-	}
-	int size = host1.length();
-	while (i < size ) {
-	    if (host1.charAt(i) == '.') {
-		break;
-	    } else {
-		i++;
-	    }
-	}
-	if (size - i == host2.length() - j) {
-	    return host1.regionMatches(i, 
-				       host2,
-				       j, 
-				       size - i);
-	} else {
-	    return false;
-	}
+    private static String getHost(String name) {
+        int pos = name.indexOf('/');
+        return (pos == -1) ? name : name.substring(pos + 1);
+    }
+
+    private static boolean compareHost(String host1, int i,
+            String host2, int j) {
+        if (host1.charAt(i) != '-') {
+            throw new IllegalArgumentException();
+        }
+        int size = host1.length();
+        while (i < size) {
+            if (host1.charAt(i) == '.') {
+                break;
+            } else {
+                i++;
+            }
+        }
+        if (size - i == host2.length() - j) {
+            return host1.regionMatches(i,
+                    host2,
+                    j,
+                    size - i);
+        } else {
+            return false;
+        }
     }
 
     // ----------------------------------
-
     /**
      * Currently not implemented.
      */
     public Oid getStringNameType()
-	throws GSSException {
-	throw new GSSException(GSSException.UNAVAILABLE);
+            throws GSSException {
+        throw new GSSException(GSSException.UNAVAILABLE);
     }
 
     /**
      * Currently not implemented.
      */
     public byte[] export()
-	throws GSSException {
-	throw new GSSException(GSSException.UNAVAILABLE);
+            throws GSSException {
+        throw new GSSException(GSSException.UNAVAILABLE);
     }
 
     /**
      * Currently not implemented.
      */
     public GSSName canonicalize(Oid mech)
-	throws GSSException {
-	throw new GSSException(GSSException.UNAVAILABLE);
+            throws GSSException {
+        throw new GSSException(GSSException.UNAVAILABLE);
     }
 
     private void writeObject(ObjectOutputStream oos) throws IOException {
@@ -353,10 +379,10 @@ public class GlobusGSSName implements GSSName, Serializable {
         oos.writeObject(name.getName());
     }
 
-    private void readObject(ObjectInputStream ois) 
-        throws IOException, ClassNotFoundException {
+    private void readObject(ObjectInputStream ois)
+            throws IOException, ClassNotFoundException {
 
-        this.nameType = (Oid)ois.readObject();
-        this.name = new X500Principal((String)ois.readObject());
+        this.nameType = (Oid) ois.readObject();
+        this.name = new X500Principal((String) ois.readObject());
     }
 }
