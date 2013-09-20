526e2d0f22e88f27a65ff80ea8f81b5e4033dff1
Venkatesh Yekkirala <ysvenkat@ncsa.illinois.edu>
2013-09-20 11:47:19 -0500

    Added documentation.

--- gridshib/gridshib-saml-tools/config/bin/gridshib-aa-test.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/env bash
-GRIDSHIB_HOME=$(cd "`dirname $0`/.." && pwd)
-java $JAVA_OPTS -Dgridshib.home=$GRIDSHIB_HOME -Dorg.globus.gridshib.config=$GRIDSHIB_HOME/gridshib-bootstrap.properties -cp "$GRIDSHIB_HOME/lib/*" org.globus.gridshib.client.saml.query.GridShibClient "$@"
